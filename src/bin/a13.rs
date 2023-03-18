d9a311a3cb60217c05d835f1a620da7c09daa563
t_hirayama_gxpinc <t.hirayama@gxp.co.jp>
2023-03-19 07:10:28 +0900

    tessoku-book a13

---
+++ src/bin/a13.rs
@@ -0,0 +1,82 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        n: usize,
+        k: u64,
+        a: [(u64); n]
+    }
+
+    let mut cnt = 0;
+    for x in 0..(n - 1) {
+        let mut l = x + 1;
+        let mut r = n - 1;
+        while l < r {
+            let m = (l + r) / 2;
+            if a[m] - a[x] > k {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        if l >= n || a[l] - a[x] > k
+        {
+            cnt += l - x - 1;
+        }
+        else
+        {
+            cnt += r - x;
+        }
+    }
+    print!("{}", cnt);
+}
