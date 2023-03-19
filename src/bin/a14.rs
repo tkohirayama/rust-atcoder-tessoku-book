4f70a672e30504d60a79756aeb5a12bbbfa7df0d
t_hirayama_gxpinc <t.hirayama@gxp.co.jp>
2023-03-19 16:48:02 +0900

    tessoku-book a14

---
+++ src/bin/a14.rs
@@ -0,0 +1,84 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        n: usize,
+        k: u64,
+        a: [(u64); n],
+        b: [(u64); n],
+        c: [(u64); n],
+        d: [(u64); n]
+    }
+
+    for ai in a {
+        if ai > k - 3 {
+            continue;
+        }
+        for bi in &b {
+            if ai + bi > k - 2 {
+                continue;
+            }
+            for ci in &c {
+                if ai + bi + ci > k - 1 {
+                    continue;
+                }
+                for di in &d {
+                    if ai + bi + ci + di == k {
+                        println!("Yes");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    println!("No");
+}
